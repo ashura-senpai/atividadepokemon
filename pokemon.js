194871ff4ff79940e430a1cc7c75786f922fc511
ashuraa <91293924+ashura-senpai@users.noreply.github.com>
2024-04-12 00:33:42 +0000

    update local storage

--- pokemon.js
+++ pokemon.js
@@ -34,10 +34,53 @@ function getEvolucaoFromURL() {
         .catch(error => console.error('Erro ao obter imagem do Pokémon:', error));
     }
   }
+
+  function updateVisitCounter() {
+    let visitData = localStorage.getItem('visitData');
+  
+    if (!visitData) {
+      visitData = { count: 0, lastVisit: '' };
+    } else {
+      visitData = JSON.parse(visitData);
+    }
+  
+    visitData.count++;
+    const currentDate = new Date();
+    
+    const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+    
+    const formattedDate = dateFormatter.format(currentDate);
+    visitData.lastVisit = formattedDate;
+    localStorage.setItem('visitData', JSON.stringify(visitData));
+
+  }
+
+  function updateFooter() {
+    const visitData = JSON.parse(localStorage.getItem('visitData'));
+  
+    if (visitData) {
+      const footerText = `Esta página foi visitada ${visitData.count} vezes. A última visita foi: ${visitData.lastVisit}`;
+  
+      const paragraph = document.createElement('p');
+      paragraph.textContent = footerText;
+  
+      const footer = document.querySelector('footer');
+      footer.appendChild(paragraph);
+    }
+  }
+
   
   function main() {
     updatePageTitleAndHeader();
     fetchPokemonData();
+    updateVisitCounter();
+    updateFooter();
   }
   
   window.onload = main;  
